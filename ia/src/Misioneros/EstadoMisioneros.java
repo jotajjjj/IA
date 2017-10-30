60a7cd9a7c15e911ed63ae349b27d0a16c748ceb
jotajjjj <josejosejjjj@hotmail.com>
2017-10-30 22:45:47 +0100

    Actualizado el estadosMisioneros

--- ia/src/Misioneros/EstadoMisioneros.java
+++ ia/src/Misioneros/EstadoMisioneros.java
@@ -25,10 +25,98 @@ public class EstadoMisioneros {
 		 }
 		 public EstadoMisioneros(int nMisioneros, int nCanibales, boolean
 		 barcaIzq) {
-		 this.nMisioneros = nMisioneros;
-		 this.nCanibales = nCanibales;
+		 this.numMisioneros = nMisioneros;
+		 this.numCanibales = nCanibales;
 		 this.barcaIzq = barcaIzq;
 		 }
+		 
+		public int getnMisioneros(){
+			
+			return this.numMisioneros;
+		}
+		public int getnCanibales(){
+			
+			return this.numCanibales;
+		}
+		public boolean isBarcaIzq(){
+			return this.barcaIzq;
+		}
 	 
-	 
+	 /*
+	  * tenemos que definir lo estador y operadores pres y post
+	  */
+		public void moveM(){
+			if(this.barcaIzq)
+				this.numMisioneros--;
+			else
+				this.numMisioneros++;
+			
+			cambiarDeOrilla();
+			
+		}
+		public void cambiarDeOrilla(){
+			
+		this.barcaIzq=(this.barcaIzq)?false:true;
+			
+		}
+		
+		
+		public boolean movimientoValido(Action where){
+			boolean valido=false;
+			
+			if(where.equals(M)){
+				if((barcaIzq&&numMisioneros>0)
+						||(!barcaIzq&& this.numMisioneros<3)){
+					EstadoMisioneros estadoSiguiente= new EstadoMisioneros(this);
+					
+					
+				}
+				else valido=false;
+				
+			}
+			else if(where.equals(MM)){
+				
+				
+			}
+			
+			return valido;
+		}
+		
+		private boolean peligro(){
+			
+			if(((this.numMisioneros<this.numCanibales)&&(
+					this.numMisioneros!=0))||((this.numMisioneros>this.numCanibales)&&
+							(this.numMisioneros!=3)))
+				return true;
+			else
+				return false;
+			
+			
+			
+		}
+		public boolean equals(Object o){
+			
+			if(this==o){
+				return true;
+			}
+			if ((o == null) || (this.getClass() != o.getClass())) {
+				return false;
+			}
+			
+			EstadoMisioneros otroEstado=(EstadoMisioneros)o;
+			
+			if((this.numMisioneros==otroEstado.getnMisioneros())&&
+					(this.numCanibales==otroEstado.getnCanibales())&&
+					(this.barcaIzq==otroEstado.barcaIzq))
+				return true;
+			else
+				return false;
+				
+				
+			
+			
+		}
+		
+		
+		
 }
