cf3facc02daf684461b129384fc1da11869573ac
jotajjjj <josejosejjjj@hotmail.com>
2017-10-30 20:07:41 +0100

    PruebaDesde mi portatil, he tenido que importar otra vez el jar

--- ia/src/Misioneros/EstadoMisioneros.java
+++ ia/src/Misioneros/EstadoMisioneros.java
@@ -3,10 +3,13 @@ package Misioneros;
 import aima.core.agent.Action;
 import aima.core.agent.impl.DynamicAction;
 
+
 public class EstadoMisioneros {
 	private int numMisioneros; // numMisioneros en orilla izquierda
     private int numCanibales; // numCanibales en orilla izquierda
 	private boolean barcaIzq; // true si barca en orilla izquierda
+	
+	
 	public static Action M = new DynamicAction("M");
 	 public static Action MM = new DynamicAction("MM");
 	 public static Action C = new DynamicAction("C");
